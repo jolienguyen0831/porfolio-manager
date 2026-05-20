5b45a03ba957fdedda3de99ebfbd67f5ac44cce8
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-05-20 10:35:01 -0500

    create House class

---
+++ src/main/java/com/pluralsight/finance/House.java
@@ -0,0 +1,31 @@
+package com.pluralsight.finance;
+
+public class House extends FixedAsset {
+    private int yearBuilt;
+    private int squareFeet;
+    private int bedrooms;
+
+    public House(String name, double marketValue, int yearBuilt, int squareFeet, int bedrooms) {
+        super(name, marketValue);
+        this.yearBuilt = yearBuilt;
+        this.squareFeet = squareFeet;
+        this.bedrooms = bedrooms;
+    }
+
+    public int getYearBuilt() {
+        return yearBuilt;
+    }
+
+    public int getSquareFeet() {
+        return squareFeet;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+}
