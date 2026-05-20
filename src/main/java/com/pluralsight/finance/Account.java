70599daa5fa300775a1610f9faacf9028353d8fd
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-05-20 10:37:01 -0500

    create abstract Account class

---
+++ src/main/java/com/pluralsight/finance/Account.java
@@ -0,0 +1,29 @@
+package com.pluralsight.finance;
+
+public abstract class Account implements Valuable {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public Account(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public abstract double getValue();
+
+
+}
